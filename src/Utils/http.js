c9076a64dac3373d2e07e13a9b619af0432deab1
Padam7890 <padamthapamoho@gmail.com>
2024-04-08 21:33:54 +0545

    new

--- src/Utils/http.js
+++ src/Utils/http.js
@@ -3,7 +3,7 @@ import axios from "axios";
  
  // Set config defaults when creating the instance
 const http = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://api-ecommerce-app-express.onrender.com/'
   });
   
   // Alter defaults after instance has been created
