8144822a928ce6cff9132e7cea0b13327d47b501
Padam7890 <padamthapamoho@gmail.com>
2024-04-29 12:26:54 +0545

    bug fuck

--- src/Pages/Category/CreateCategory.jsx
+++ src/Pages/Category/CreateCategory.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useFormik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import { mixed, object, string } from "yup";
