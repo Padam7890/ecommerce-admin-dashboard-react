9f34b18a6e2dbb559c0a4145843d259244ddde7e
Padam7890 <padamthapamoho@gmail.com>
2024-08-12 12:46:14 +0545

    new changes

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -9,11 +9,6 @@ const Layout = () => {
       <NewSidebar />
       <div className=" flex-1 p-4 bg-[#F1F5F9] overflow-y-auto">
         <Outlet />
-        {/* <footer>
-        <p className=" ml-5 text-sm text-gray-400 mb-7">
-          Copyright © BY <a className=" text-blue-600" href="https://padamthapa.com.np/">Padam Thapa.</a> All rights reserved.
-        </p>
-      </footer> */}
       </div>
     
     </div>
